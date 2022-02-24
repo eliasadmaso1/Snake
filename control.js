ec18e81baa3c00b7fc0c64120594bb819416625d
eliasadmaso1 <eliasadmas01@gmail.com>
2022-02-24 23:06:28 +0200

    media-query

--- control.js
+++ control.js
@@ -1,6 +1,32 @@
 let inputDirection = { x: 0, y: 0 };
 let lastInputDirection = { x: 0, y: 0 };
 
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
+up.addEventListener("click", function () {
+  if (lastInputDirection.y !== 0) return;
+
+  inputDirection = { x: 0, y: -1 };
+});
+down.addEventListener("click", function () {
+  if (lastInputDirection.y !== 0) return;
+
+  inputDirection = { x: 0, y: 1 };
+});
+left.addEventListener("click", function () {
+  if (lastInputDirection.x !== 0) return;
+
+  inputDirection = { x: -1, y: 0 };
+});
+right.addEventListener("click", function () {
+  if (lastInputDirection.x !== 0) return;
+
+  inputDirection = { x: 1, y: 0 };
+});
+
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowUp":
